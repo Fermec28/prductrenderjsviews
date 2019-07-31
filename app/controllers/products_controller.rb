ec079c284bff11c6d821234d52998410aee6073c
Jose Medina <fermed28@gmail.com>
2019-07-31 13:45:24 -0500

    [add] simple controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,36 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def create
+    product = Product.new(product_params)
+    if product.save
+      redirect_to root_path
+    end
   end
 
   def update
+    product = Product.find(params[:id])
+    product.update(product_params)
   end
 
   def new
+    @product = Product.new
   end
 
   def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    redirect_to root_path
   end
 
   def edit
+    @product = Product.find(params[:id])
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name,:price)
   end
 end
