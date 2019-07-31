648fa529ae5da9dc221f2a6ec2d3b4e3a88c00d4
Jose Medina <fermed28@gmail.com>
2019-07-31 13:38:35 -0500

    [add] product controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,19 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+end
